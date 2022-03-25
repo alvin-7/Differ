6c5890268b8f3401aeed65a1fb2598afb50985f8
wenzhen.alvin <wenzhen.alvin@bytedance.com>
2022-03-26 00:51:48 +0800

    mod: preload

--- src/preload.js
+++ src/preload.js
@@ -5,24 +5,24 @@ import fs from 'fs'
 contextBridge.exposeInMainWorld('electronAPI', {
   ipcRenderer,
   readXlsx: (path) => {
-    console.log('path22', path, typeof path)
     const sheets = []
     let excelPaths = path === '' ? [] : [path]
     if (path === '') {
-      // const argvLen = process.argv.length
-      // console.log(process.argv)
-      // for (const arg of process.argv) {
-      // }
-      // if (argvLen <= 2) return sheets
-      // excelPaths = process.argv.slice(argvLen-2, argvLen)
-      return sheets
+      const argv = process.argv
+      console.log('argv', argv)
+      for (let i=0; i<argv.length; i++) {
+        if (argv[i] === '--excel') {
+          excelPaths.push(argv[i+1])
+          excelPaths.push(argv[i+2])
+          break
+        }
+      }
+      if (!excelPaths.length) return sheets
     }
     for (let path of excelPaths) {
-      console.log('1',path)
       if (typeof path === 'string') {
         path = fs.readFileSync(path)
       }
-      console.log('2',path)
       sheets.push(xlsx.parse(path))
     }
     return sheets
