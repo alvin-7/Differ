ec5eb5d970aca308e0f741421f9ef2fde9f5a5f6
alvin <zhuye.life@gmail.com>
2022-04-04 12:01:59 +0800

    add: electron update

---
+++ forge.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  packagerConfig: {},
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'alvin-7',
+          name: "Differ"
+        }
+      }
+    }
+  ],
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    [
+      '@electron-forge/plugin-webpack',
+      {
+        mainConfig: './webpack.main.config.js',
+        renderer: {
+          config: './webpack.renderer.config.js',
+          entryPoints: [
+            {
+              html: './src/renderer/index.html',
+              js: './src/renderer/index.tsx',
+              name: 'main_window',
+              preload: {
+                js: './src/preload.js',
+              },
+            },
+          ],
+        },
+        port: 7777
+      },
+    ],
+  ],
+};
